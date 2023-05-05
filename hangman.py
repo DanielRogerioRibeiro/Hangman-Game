705a600e9b80ea7ff7e5fae49d2f50883cfddc79
DanielRogerioRibeiro <danielrogerioribeiro.dev@gmail.com>
2023-05-04 22:14:53 -0300

    Creating the while condition

--- hangman.py
+++ hangman.py
@@ -2,9 +2,28 @@
 #Neste jogo o objetivo é acertar todas as letras sem ser enforcado
 
 
+print("*********************************")
+print("***Bem vindo ao jogo da Forca!***")
+print("*********************************")
 
+palavra_secreta = "banana"
 
-print ("*******SEJA BEM VINDO AO JOGO DA FORCA**********") 
+enforcou = False
+acertou = False
 
+while(not enforcou and not acertou):
 
+    chute = input("Qual letra? ")
+    chute = chute.strip()
 
+    index = 0
+    for letra in palavra_secreta:
+        if(chute.upper() == letra.upper()):
+            print("Encontrei a letra {} na posição {}".format(letra, index))
+        index = index + 1
+
+    print("jogando ...")
+
+print("Fim do jogo")
+
+ 
