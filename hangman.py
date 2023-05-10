84880dfa3b0cfb2893ba4de281c7e5ee9f035817
DanielRogerioRibeiro <danielrogerioribeiro.dev@gmail.com>
2023-05-09 21:00:40 -0300

    Removing the play function, including victory or defeat information for the user and leaving the letters always in uppercase.

--- hangman.py
+++ hangman.py
@@ -1,43 +1,44 @@
 #Este é o jogo da Forca
 #Neste jogo o objetivo é acertar todas as letras sem ser enforcado
 
-def jogar():
-    print("*********************************")
-    print("***Bem vindo ao jogo da Forca!***")
-    print("*********************************")
 
-    palavra_secreta = "mulher".upper()
-    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+print("*********************************")
+print("***Bem vindo ao jogo da Forca!***")
+print("*********************************")
 
-    enforcou = False
-    acertou  = False
-    erros    = 0
+palavra_secreta = "mulher".upper()
+letras_acertadas = ["_" for letra in palavra_secreta]
 
+enforcou = False
+acertou  = False
+erros    = 0
+
+print(letras_acertadas)
+
+while(not enforcou and not acertou):
+
+    chute = input("Qual letra? ")
+    chute = chute.strip().upper()
+
+        
+    if (chute in palavra_secreta):
+        index = 0
+        for letra in palavra_secreta:
+            if(chute == letra):
+                    letras_acertadas[index] = letra
+            index += 1
+    else:
+        erros += 1 
+        
+    enforcou = erros == 5
+    acertou = "_" not in letras_acertadas
     print(letras_acertadas)
 
-    while(not enforcou and not acertou):
-
-        chute = input("Qual letra? ")
-        chute = chute.strip().upper()
-
-        
-        if (chute in palavra_secreta):
-            index = 0
-            for letra in palavra_secreta:
-                if(chute.upper() == letra.upper()):
-                    letras_acertadas[index] = letra
-                index += 1
-        else:
-            erros += 1 
-        
-        enforcou = erros == 10
-        print(letras_acertadas)
-
-    print("Fim do jogo")
+if(acertou):
+    print("Você ganhou!!")
+else:
+    print("Você perdeu!!")
+    
+print("Fim do jogo")
 
 
-if(__name__ == "__main__"):
-    jogar()
-
-
-#modulo 5 - Palavra secreta e chute sempre em caixa alta
